78ad1620fec4a83e9ae6f29ff6906e10f54de2ca
Рита Пономарёва <ponomarevarita11@gmail.com>
2026-09-13 22:43:59 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿// // // Console.WriteLine("Hello, World!");
+
+
+// // string myName = "Рита Пономарёва";
+// // string groupName = "ИСП-251";
+// // int courseNumber = 2;
+// // double averageGrade = 4.6;
+// // bool isBurget = true;
+
+// // Console.WriteLine("Знакомство");
+// // Console.WriteLine($"Студент: {myName}");
+// // Console.WriteLine($"Группа: {groupName}");
+// // Console.WriteLine($"Курс: {courseNumber}");
+// // Console.WriteLine($"Средний балл: {averageGrade}");
+// // Console.WriteLine($"Бюджетное место: {isBurget}");
+
+// Console.WriteLine();
+// Console.WriteLine("Визитная карточка студента");
+
+// string name = "Рита Пономарёва Алексеевна";
+// const string group = "ИСП-251";
+// int course = 2;
+// string specialization = "09.02.11";
+
+// int firstWork = 4;
+// int secondWork = 5;
+// int threeWork = 4;
+
+// int work = (firstWork + secondWork + threeWork) / 3;
+
+// bool sholaship = true;
+
+// int allWeek = 18;
+// int lastWeek = 2;
+
+// int week = allWeek - lastWeek;
+
+// Console.WriteLine($"ФИO: {name}");
+// Console.WriteLine($"Группа: {group}");
+// Console.WriteLine($"Курс: {courseNumber}");
+// Console.WriteLine($"Специальность: {specialization}");
+// Console.WriteLine($"Средний балл за 3 работы: {work}");
+// Console.WriteLine($"Стипендия положена (>= 4.0): {sholaship}");
+// Console.WriteLine($"Учебных недель осталось в семестре: {week}");
+
+
+
+
+string name = "Рита Пономарёва Алексеевна";
+const string group = "ИСП-251";
+int course = 2;
+string specialization = "09.02.11";
+
+int firstWork = 4;         
+int secondWork = 5;
+int threeWork = 4;
+
+int work = (firstWork + secondWork + threeWork) / 3;
+
+bool sholaship = true;
+
+int allWeek = 18;
+int lastWeek = 2;
+
+int week = allWeek - lastWeek;
+
+Console.WriteLine("Визитная карточка студента");
+Console.WriteLine($"ФИO: {name}");
+Console.WriteLine($"Группа: {group}");
+Console.WriteLine($"Курс: {course}");
+Console.WriteLine($"Специальность: {specialization}");
+Console.WriteLine();
+Console.WriteLine($"Средний балл за 3 работы: {work}");
+Console.WriteLine($"Стипендия положена (>= 4.0): {sholaship}");
+Console.WriteLine($"Учебных недель осталось в семестре: {week}");
+Console.WriteLine($"");
